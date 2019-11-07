8750258291868b321863663ba3f7b5dd0ba65981
saurav <garg.saurav08@gmail.com>
2019-11-07 13:41:09 +0530

    database design updated (some minor changes)

--- splitwiseclone/splitapp/models.py
+++ splitwiseclone/splitapp/models.py
@@ -10,8 +10,6 @@ class UserProfile(models.Model):
     name = models.CharField(max_length=30)
     profile_pic = models.ImageField(default='default.png', null=True, blank=True)
     password = models.CharField(max_length=30)
-    groups = models.CharField(validators=[int_list_validator], max_length=1000, null=True, blank=True)
-    friends = models.CharField(validators=[int_list_validator], max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.user_name
@@ -19,18 +17,6 @@ class UserProfile(models.Model):
 
     class Meta:
         db_table = 'UserProfile'
-
-
-class Group(models.Model):
-    group_id = models.AutoField(primary_key=True)
-    group_name = models.CharField(max_length=30)
-    users = models.CharField(validators=[int_list_validator],max_length=1000, blank=True)
-
-    def __str__(self):
-        return self.group_name
-
-    class Meta:
-        db_table = 'Groups'
 
 
 class UserGroup(models.Model):
@@ -57,8 +43,8 @@ class UserFriend(models.Model):
 
 class Transaction(models.Model):
     transaction_id = models.AutoField(primary_key=True)
-    lender = models.IntegerField()
-    borrower = models.IntegerField()
+    lender = models.CharField(max_length=30)
+    borrower = models.CharField(max_length=30)
     group_id = models.IntegerField()
     amount = models.IntegerField()
     date_time = models.DateTimeField(auto_now_add=True)
